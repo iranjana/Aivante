d912205f7afd56aa5e5960e37b8c9f3c1203a2ff
Ranjana <ranjna.krishna@gmail.com>
2025-03-12 10:09:25 -0600

    Update DialysisFacilities.cy.js
    Added more codes in it

--- Pages/DialysisFacilities.cy.js
+++ Pages/DialysisFacilities.cy.js
@@ -1,10 +1,27 @@
 class DialysisFacilities{
+    dialysisfacilitynameEle = ('#mat-input-14');
+    streetEle = ('#mat-input-15');
     dialysisEle =(':nth-child(8) > .category-name');
     zipcodeEle =('#mat-input-16');
     zipsearchEle =('.mat-button-wrapper > .mat-icon');
-    radiusinEle =('#mat-input-18');
+    cityEle = ('.mat-form-field.ng-tns-c170-53 > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix');
+     citynameEle = ('#mat-option-121 > .mat-option-text');
+    radiusinEle = ('#mat-input-18');
     searchEle = ('.display-flex > .mat-focus-indicator > .mat-button-wrapper');
+    providerfilterEle = ('#mat-expansion-panel-header-2 > .mat-expansion-indicator');
+    distanceEle = ('#distance');
+    ratingEle = (' #rating_patient > .mat-select-trigger > .mat-select-arrow-wrapper > .mat-select-arrow');
+    ratingfiveEle = ('#mat-option-136');
+    applyfilterEle = (' :nth-child(2) > [type="submit"]');
+    clearfilterEle = ('form.ng-valid > :nth-child(2) > [type="button"] > .mat-button-wrapper');
    backbtnEle = ('.button-wrapper > .mat-focus-indicator > .mat-button-wrapper');
+
+    enterDialysisFacilityName(dialysisfacilityname) {
+        cy.get(this.dialysisfacilitynameEle).type(dialysisfacilityname);
+    }
+enterStreet(street) {
+        cy.get(this.streetEle, { timeout: 3000 }).type(street);
+    }
 
     clickDialysis(){
         cy.get(this.dialysisEle).click();
@@ -15,12 +32,36 @@ class DialysisFacilities{
     clickZipSearch(){
         cy.get(this.zipsearchEle).click();
     }
+     clickCity(){
+        cy.get(this.cityEle).click();
+    }
+    selectCityName() {
+        cy.get(this.citynameEle).click();
+    }
     clickRadiusIn(){
         cy.get(this.radiusinEle).click();
     }
-
     clickSearch(){
         cy.get(this.searchEle).click();
+    }
+    clickProviderFilter() {
+        cy.get(this.providerfilterEle).click();
+    }
+    enterDistance(distance) {
+        cy.get(this.distanceEle).type(distance);
+    }
+    clickRating() {
+        cy.get(this.ratingEle).click();
+    }
+    selectRatingFive() {
+        cy.get(this.ratingfiveEle).click();
+    }
+
+    clickApplyFilter() {
+        cy.get(this.applyfilterEle).click({ force: true });
+    }
+    clickClearFilter() {
+        cy.get(this.clearfilterEle).click({ force: true });
     }
     clickBackBtn(){
         cy.get(this.backbtnEle).click();

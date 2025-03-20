ee4246d1f46a91542605347531ec0ce1d601d087
Ranjana <ranjna.krishna@gmail.com>
2025-03-20 11:55:43 -0600

    Added it block
    it blocks

--- Test/ProviderDialysisFacilityTest.cy.js
+++ Test/ProviderDialysisFacilityTest.cy.js
@@ -1,37 +1,98 @@
-describe('Automation testing', () => {
-    it('test', () => {
-
+import ProviderDialysisFacility from "../Pages/ProviderDialysisFacility.cy";
+describe('providerdialysisfacility', () => {
+    beforeEach(() => {
         cy.visit('http://169.61.105.110/medicareAdvantage_sandbox/landing-page ');
-        cy.fixture('LoginFixture').then((data)=>{
+        cy.fixture('LoginFixture').then((data) => {
 
-    cy.wait(3000)
-    const providerDialysisFacility = new providerDialysisFacility();
-    dialysisFacilities.enterDialysisFacilityName("Davita Englewood Dialysis Center");
-    dialysisFacilities.enterStreet("Girard");
-    dialysisFacilities.clickDialysis();
-    dialysisFacilities.enterZipCode("80113");
-    cy.wait(1000)
-    dialysisFacilities.clickZipSearch();
-    dialysisFacilities.clickCity();
-    dialysisFacilities.selectCityName();
-    dialysisFacilities.clickRadiusIn();
-    dialysisFacilities.clickSearch();
-    cy.wait(1000)
-    dialysisFacilities.clickProviderFilter();
-    cy.wait(1000)
-    dialysisFacilities.enterDistance(10);
-    cy.wait(1000)
-    dialysisFacilities.clickRating();
-    cy.wait(1000)
-    dialysisFacilities.selectRatingFive();
-    cy.wait(1000)
-    dialysisFacilities.clickApplyFilter();
-    cy.wait(1000)
-    dialysisFacilities.clickClearFilter();
-    cy.wait(1000)
-    dialysisFacilities.clickBackBtn();
+            it('TC-1 dialysisfacility name', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacility.enterDialysisFacilityName("Davita Englewood Dialysis Center");
+            });
+
+            it('TC-2 dialysisfacility street', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacilities.enterStreet("Girard");
+            });
+
+            it('TC-3 dialysisfacility clickdialysis', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacilities.clickDialysis();
+            });
+
+            it('TC-4 dialysisfacility centerzipcode', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacilities.enterZipCode("80113");
+            });
+
+            it('TC-5 dialysisfacility clickzip search', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.clickZipSearch();
+            });
+
+            it('TC-6 dialysisfacility clickcity', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacilities.clickCity();
+            });
+
+            it('TC-7 dialysisfacilitys electcityname', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacilities.selectCityName();
+            });
+
+            t('TC-8 dialysisfacility clickradius', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacilities.clickRadiusIn();
+            });
+
+            it('TC-9 dialysisfacility clicksearch', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                dialysisFacilities.clickSearch();
+            });
+
+            it('TC-10 dialysisfacility clickproviderfilter', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.clickProviderFilter();
+            });
+
+            it('TC-11 dialysisfacility enterdistance', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.enterDistance(10);
+            });
+
+            it('TC-11 dialysisfacility clickrating', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.clickRating();
+            });
+
+            it('TC-12 dialysisfacility select ratingfive', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.selectRatingFive();
+            });
+
+            it('TC-13 dialysisfacility clickapply filter', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.clickApplyFilter();
+            });
+
+            it('TC-13 dialysisfacility clickclear filter', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.clickClearFilter();
+            });
+
+            it('TC-14 dialysisfacility clickback button', () => {
+                const providerDialysisFacility = new ProviderDialysisFacility();
+                cy.wait(1000)
+                dialysisFacilities.clickBackBtn();
+            });
 
         })
-              
-    } )
-    })      
+
+    })
+})      
